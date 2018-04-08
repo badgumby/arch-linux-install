a7da297a06f4224d88d404f1aeb2ba665a7e4c57
BAD Gumby <badgumby@noreply.github.com>
2018-04-08 10:37:57 -0500

    added icons, dconf-editor options, etc

--- arch-install-gumby.sh
+++ arch-install-gumby.sh
@@ -147,3 +147,29 @@ pacaur -S ant-nebula-gtk-theme candy-gtk-themes paper-icon-theme ttf-material-ic
 
 # Add mate-menu if you want the advanced menu
 pacaur -S mate-menu
+
+###################################################
+# dconf-editor changes
+/org/mate/panel/menubar/icon-name
+#arch-24 (copy icons to /home/*user*/.icons/)
+
+/org/gnome/login-screen/banner-message-text
+#Set if you want
+
+/org/mate/desktop/interface
+#Set themes
+
+/org/mate/caja/preferences
+#always-use-location-entry: on
+
+/org/mate/power-manager
+#backlight-battery-reduce: off
+#button-lic-ac: nothing
+#button-lid-battery: nothing
+#button-power: interactive
+#button-suspend: nothing
+#idle-dim-battery: off
+###################################################
+
+# Set MATE defaults
+mate-default-applications-properties
---
+++ icons/README.md
@@ -0,0 +1,5 @@
+# Icons
+Icons in my base directory
+
+###### Place in the following location
+> /home/*user*/.icons/
