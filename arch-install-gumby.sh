1e84218c4008429f3dce31a0ee8b75023f0e66a2
BADGumby <badgumby@users.noreply.github.com>
2018-04-07 22:01:07 -0500

    added a bunch of stuff

--- arch-install-gumby.sh
+++ arch-install-gumby.sh
@@ -1,12 +1,7 @@
-###################################################
-###################################################
-## Do not execute this, as it is just a template ##
-###################################################
-###################################################
-# Install ARCH Linux with encrypted file-system and UEFI
+# Install ARCH Linux with encrypted file-system
+# Options for EFI or BIOS
 # The official installation guide (https://wiki.archlinux.org/index.php/Installation_Guide) contains a more verbose description.
 # Modified by BAD Gumby
-
 
 # Download the archiso image from https://www.archlinux.org/
 # Copy to a usb-drive
@@ -18,10 +13,22 @@ dd if=archlinux.img of=/dev/sdX bs=16M && sync # on linux
 wifi-menu
 
 # Create partitions
-cgdisk /dev/sdX
+fdisk -l
+echo "Enter your storage device (ex. For /dev/sda enter sda): "
+read STORAGEDEVICE
+cgdisk /dev/$STORAGEDEVICE
+
+#############################################################################
+# For EFI, use this
 1 100MB EFI partition # Hex code ef00
 2 250MB Boot partition # Hex code 8300
 3 100% size partiton # (to be encrypted) Hex code 8300
+
+# For BIOS, use this
+1 100MB BIOS partition # Hex code ef02
+2 250MB Boot partition # Hex code 8300
+3 100% size partiton # (to be encrypted) Hex code 8300
+#############################################################################
 
 mkfs.vfat -F32 /dev/sdX1
 mkfs.ext2 /dev/sdX2
@@ -41,7 +48,7 @@ lvcreate -l +100%FREE vg0 --name root
 mkfs.ext4 /dev/mapper/vg0-root
 mkswap /dev/mapper/vg0-swap
 
-# Mount the new system 
+# Mount the new system
 mount /dev/mapper/vg0-root /mnt # /mnt is the installed system
 swapon /dev/mapper/vg0-swap # Not needed but a good thing to test
 mkdir /mnt/boot
@@ -49,10 +56,15 @@ mount /dev/sdX2 /mnt/boot
 mkdir /mnt/boot/efi
 mount /dev/sdX1 /mnt/boot/efi
 
-# Install the system also includes stuff needed for starting wifi when first booting into the newly installed system
-pacstrap /mnt base base-devel grub-efi-x86_64 zsh vim git efibootmgr dialog wpa_supplicant xf86-video-intel xorg-server xorg-apps gdm mate mate-extra bluez-utils
+##########################################################################################################################################################
+# For EFI, use this
+pacstrap /mnt base base-devel grub-efi-x86_64 efibootmgr zsh vim git dialog wpa_supplicant xf86-video-intel xorg-server xorg-apps gdm mate mate-extra bluez-utils
 
-# 'install' fstab
+# For BIOS, instead of EFI use this
+pacstrap /mnt base base-devel grub-bios zsh vim git dialog wpa_supplicant xf86-video-intel xorg-server xorg-apps gdm mate mate-extra bluez-utils
+##########################################################################################################################################################
+
+# Write fstab
 genfstab -pU /mnt >> /mnt/etc/fstab
 # Make /tmp a ramdisk (add the following line to /mnt/etc/fstab)
 tmpfs	/tmp	tmpfs	defaults,noatime,mode=1777	0	0
@@ -67,7 +79,9 @@ ln -s /usr/share/zoneinfo/America/Chicago /etc/localtime
 hwclock --systohc --utc
 
 # Set the hostname
-echo MYHOSTNAME > /etc/hostname
+echo "Enter hostname: "
+read MYHOSTNAME
+echo $MYHOSTNAME > /etc/hostname
 
 # Enable locale(s)
 vim /etc/locale.gen
@@ -80,7 +94,9 @@ echo LC_ALL=en_US.UTF-8 >> /etc/locale.conf
 passwd
 
 # Add real user remove -s flag if you don't whish to use zsh
-# useradd -m -g users -G wheel MYUSERNAME
+echo "Enter username: "
+read $MYHOSTNAME
+useradd -m -g users -G wheel $MYUSERNAME
 # passwd MYUSERNAME
 
 # Configure mkinitcpio with modules needed for the initrd image
@@ -94,9 +110,9 @@ mkinitcpio -p linux
 
 # Setup grub
 grub-install
-# In /etc/default/grub edit the line GRUB_CMDLINE_LINUX to:
-GRUB_CMDLINE_LINUX="cryptdevice=/dev/sdX3:luks:allow-discards" 
-# then run:
+# In /etc/default/grub edit the line GRUB_CMDLINE_LINUX to
+GRUB_CMDLINE_LINUX="cryptdevice=/dev/sdX3:luks:allow-discards"
+# then run
 grub-mkconfig -o /boot/grub/grub.cfg
 
 # Enable desired services
@@ -117,8 +133,17 @@ reboot
 # Install pacaur
 https://github.com/rmarquis/pacaur
 
-# Install after pacaur
-pacaur -S mate-tweak mate-media system-config-printer network-manager-applet oh-my-zsh-git neovim-gtk-git
+# Install base tools
+pacaur -S mate-tweak mate-media system-config-printer network-manager-applet oh-my-zsh-git neovim-gtk-git aic94xx-firmware wd719x-firmware
+
+# Install the needful
+pacaur -S atom google-chrome filezilla tilda-git remmina remmina-plugin-rdesktop visual-studio-code-bin wps-office jdk8 jre8 scrot nmap
+
+# Install bonus apps
+pacaur -S ncmpcpp mopidy mopidy-gmusic keybase-bin signal-desktop-bin multibootusb neofetch correcthorse skype-electron zoom steam gimp inkscape
+
+# Install themes/fonts
+pacaur -S ant-nebula-gtk-theme candy-gtk-themes paper-icon-theme ttf-material-icons ttf-ms-fonts ttf-wps-fonts typecatcher
 
 # Add mate-menu if you want the advanced menu
 pacaur -S mate-menu
