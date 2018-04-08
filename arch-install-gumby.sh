c6a3e33bec2434510f4a7ca93fa35fe4e78c2d0f
BAD Gumby <badgumby@noreply.github.com>
2018-04-08 10:48:08 -0500

    split configuration to .md file

--- arch-install-gumby.sh
+++ arch-install-gumby.sh
@@ -144,32 +144,3 @@ pacaur -S ncmpcpp mopidy mopidy-gmusic keybase-bin signal-desktop-bin multibootu
 
 # Install themes/fonts
 pacaur -S ant-nebula-gtk-theme candy-gtk-themes paper-icon-theme ttf-material-icons ttf-ms-fonts ttf-wps-fonts typecatcher
-
-# Add mate-menu if you want the advanced menu
-pacaur -S mate-menu
-
-###################################################
-# dconf-editor changes
-/org/mate/panel/menubar/icon-name
-#arch-24 (copy icons to /home/*user*/.icons/)
-
-/org/gnome/login-screen/banner-message-text
-#Set if you want
-
-/org/mate/desktop/interface
-#Set themes
-
-/org/mate/caja/preferences
-#always-use-location-entry: on
-
-/org/mate/power-manager
-#backlight-battery-reduce: off
-#button-lic-ac: nothing
-#button-lid-battery: nothing
-#button-power: interactive
-#button-suspend: nothing
-#idle-dim-battery: off
-###################################################
-
-# Set MATE defaults
-mate-default-applications-properties
---
+++ configuration.md
@@ -0,0 +1,25 @@
+# Configuration changes after install
+
+##### dconf-editor changes
+###### /org/mate/panel/menubar/icon-name
+> arch-24 (copy icons to /home/*user*/.icons/)
+
+###### /org/gnome/login-screen/banner-message-text
+> Set if you want
+
+###### /org/mate/desktop/interface
+> Set themes
+
+###### /org/mate/caja/preferences
+> always-use-location-entry: on
+
+###### /org/mate/power-manager
+> backlight-battery-reduce: off
+> button-lic-ac: nothing
+> button-lid-battery: nothing
+> button-power: interactive
+> button-suspend: nothing
+> idle-dim-battery: off
+
+#### Set MATE defaults
+###### mate-default-applications-properties
